6d00768b7d3cab27aae8c93e58be2a654ec405fb
Jan Morales <moralesjan40@gmail.com>
2021-10-04 14:21:44 +0000

    Jan completed 1-6

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
-//1
+
 function countToTen(){
-  
+    for( let i = 0; i <= 10; i++){
+      console.log(i)
+}
+}
+// countToTen()
+
+
+function countFromOne(num){
+  for( let i = 1; i <= num; i++){
+      console.log(i)
+  }
+}
+// countFromOne(13)
+
+function countEveryOdd(num){
+  for( let i = 1; i <= num; i+=2){
+      console.log(i)
+  }
+}
+// countEveryOdd(13)
+
+
+function isNegative(num){
+  if (num < 0){
+  return true
+  }else{
+    return false
+  }
+}
+// console.log(isNegative(9))
+
+function betweenFiveAndTwenty(num){
+    if (5 <= num && num <= 20){
+        return true;
+    }else{
+        return false;
+    }
+}
+// console.log(betweenFiveAndTwenty(10))
+
+function isAllLowerCase(string){
+   const lowercase = string.toLowerCase()
+   if(lowercase === string){
+       return true
+   }else{
+       return false
+   }
 }
 
-//2
-function countFromOne(){
-  
-}
-
-//3
-function countEveryEven(){
-  
-}
-
-//4
-function countEveryOdd(){
-  
-}
-
-//5 
-function countEvens(){
-  
-}
-
+// console.log(isAllLowerCase("eleveEEn"))
